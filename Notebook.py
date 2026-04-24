6506be2739a0494f12f84fbf80d2178c76643a5d
hleva1 <hleva19890203@gmail.com>
2026-04-24 20:33:26 +0300

    new file

--- Notebook.py
+++ Notebook.py
@@ -31,7 +31,8 @@ while True:
         new_note = str(input("Enter a new note : "))
         notes[index] = new_note
 
-        # 2 По повному збігу старого напису kjkkkkkkkk
+        # 2 По повному збігу старого напису kjkkkkkkkkkkkkkk
+
     elif command == "u2":
         old_note = str(input("Enter a note : "))
         new_note = str(input("Enter a new note : "))
